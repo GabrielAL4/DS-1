f195dc3a35d96e26de6a259662803063c4ef2c56
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-12-14 18:32:26 -0300

    refactor: remove opcoes desnecessarias para filtro de hora

--- ds1-2025.1/src/app/turmas/page.jsx
+++ ds1-2025.1/src/app/turmas/page.jsx
@@ -765,9 +765,6 @@ export default function AlocarTurmaSala() {
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
                 </select>
                 <div className="flex items-center gap-2 flex-wrap">
                   <CriarTurmaModal setTabela={setTabela} />
